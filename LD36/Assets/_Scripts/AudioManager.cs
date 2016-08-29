c8b5850385d14656036d261972b28b91635423df
Recursiveanomaly <marnelltom@gmail.com>
2016-08-29 09:39:35 -0600

    add mute buttons

--- LD36/Assets/_Scripts/AudioManager.cs
+++ LD36/Assets/_Scripts/AudioManager.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
     public AudioSource m_audioSource;
+    public AudioSource m_audioSourceMusic;
+
+    public Text m_muteMusic;
+    public Text m_muteSFX;
 
     public AudioClip UIclick;
     public AudioClip guessedWrong;
@@ -60,5 +65,34 @@ public class AudioManager : SingletonMonoBehaviour<AudioManager>
     {
         m_audioSource.clip = switchTablet;
         m_audioSource.Play();
+    }
+
+    public void ToggleMuteMusic()
+    {
+        m_audioSourceMusic.mute = !m_audioSourceMusic.mute;
+        m_audioSourceMusic.Play();
+
+        if(m_audioSourceMusic.mute)
+        {
+            m_muteMusic.text = "Enable Music";
+        }
+        else
+        {
+            m_muteMusic.text = "Mute Music";
+        }
+    }
+
+    public void ToggleMuteSFX()
+    {
+        m_audioSource.mute = !m_audioSource.mute;
+
+        if (m_audioSource.mute)
+        {
+            m_muteSFX.text = "Enable SFX";
+        }
+        else
+        {
+            m_muteSFX.text = "Mute SFX";
+        }
     }
 }
